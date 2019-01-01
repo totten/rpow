e65fb04c486fa26a2509e9d5e73635e522f52593
Tim Otten <totten@civicrm.org>
2018-12-31 23:31:45 -0800

    Add helper for civicrm.settings.php (include/init.php)

---
+++ include/init.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Initialize civirpow - setup class-loading and apply the configuration
+ * options.
+ *
+ * This helper is intended for use in
+ *
+ * @param array $config
+ *   Ex: [
+ *    'slaves' => ['mysql://ro_user:ro_pass@ro_host/ro_db?new_link=true'],
+ *    'masters' => [mysql://rw_user:rw_pass@rw_host/rw_db?new_link=true'],
+ *   ])
+ */
+function civirpow_init($config = []) {
+  // Native classes
+  require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+  // PEAR DB classes
+  set_include_path(dirname(__DIR__) . PATH_SEPARATOR . get_include_path());
+
+  global $civirpow;
+  $civirpow = $config;
+
+  define('CIVICRM_DSN', 'civirpow://');
+  // define('CIVICRM_DSN', $config['masters'][0]);
+  // define('CIVICRM_DSN', $config['slaves'][0]);
+}
